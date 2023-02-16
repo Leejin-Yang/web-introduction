d475271665485f9c5d60757dff2f2a0974f3f405
Leejin-Yang <ejin1996@gmail.com>
2023-02-17 01:46:51 +0900

    feat: 방명록 취소 버튼 기능

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const visitorNameInput = document.querySelector('#visitor-name');
 const movieCheckboxes = document.querySelectorAll('.movie-checkbox');
 const registerButton = document.querySelector('#visitor-register-button');
+const resetButton = document.querySelector('#visitor-reset-button');
 
 const onClickRegisterButton = () => {
   const name = visitorNameInput.value;
@@ -11,4 +12,10 @@ const onClickRegisterButton = () => {
   alert(`${name}님, 저와 ${movieCheckedCount}개의 취향이 같으시네요!`);
 };
 
+const onClickResetButton = () => {
+  visitorNameInput.value = '';
+  movieCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+};
+
 registerButton.addEventListener('click', onClickRegisterButton);
+resetButton.addEventListener('click', onClickResetButton);
